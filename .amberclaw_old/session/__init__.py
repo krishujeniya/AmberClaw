1632eb1127b545f408e4fffac7fa8a7623d57675
krishujeniya <krishujeniya@gmail.com>
2026-05-05 12:44:05 +0530

    chore: remove .amberclaw_old and .planning from git tracking

--- .amberclaw_old/session/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Session management module."""
-
-from amberclaw.session.manager import Session, SessionManager
-
-__all__ = ["SessionManager", "Session"]
--- .amberclaw_old/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions for amberclaw."""
-
-from amberclaw.utils.helpers import ensure_dir
-
-__all__ = ["ensure_dir"]
